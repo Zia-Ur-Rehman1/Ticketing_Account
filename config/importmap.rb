ecee0522f23f5372fa24b591f5c53e89ae76e632
Zia-Ur-Rehman1 <ziakhanalone100@gmail.com>
2023-11-06 15:11:27 +0500

    Added Customer Supplier datalist in Add New record

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "jquery", to: "jquery.min.js", preload: true
 pin "jquery_ujs", to: "jquery_ujs.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/controllers", under: "controllers", preload: true
